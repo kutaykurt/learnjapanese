bb2bb0a7289a75235a6d36fd908f5319a6ab21cd
kutaykurt <Kutay_Kurt@gmx.de>
2023-12-19 08:24:39 +0100

    Verbesserungen beim Design

--- src/pages/Homepage/Hiragana/Alphabet/Alphabet.jsx
+++ src/pages/Homepage/Hiragana/Alphabet/Alphabet.jsx
@@ -1,6 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { fetchJapaneseData } from '../../../../fetch';
-import { Dropdown } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import { VocabularyContext } from '../../../../components/VocabularyProvider';
 
