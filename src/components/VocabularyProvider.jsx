92ff27ebb8dada4a0b91026edd40831f92937e8e
kutaykurt <Kutay_Kurt@gmx.de>
2023-12-21 03:41:33 +0100

    Das Hinzufügen aller Vokabeln funktioniert jetzt. Es treten keine Fehler mehr auf

--- src/components/VocabularyProvider.jsx
+++ src/components/VocabularyProvider.jsx
@@ -6,22 +6,22 @@ export const VocabularyProvider = ({ children }) => {
   const [vocabularyList, setVocabularyList] = useState([]);
   const [selectedVocabularies, setSelectedVocabularies] = useState([]);
 
+  const generateVocabularyId = (vocabulary) => {
+    return `${vocabulary.character}-${vocabulary.japanese}-${vocabulary.pronunciation}-${vocabulary.translation.german}-${vocabulary.translation.english}`;
+  };
+
   const addVocabulary = (newVocabulary) => {
-    // Überprüfe, ob die Vokabel bereits in der Liste vorhanden ist
+    const newVocabularyId = generateVocabularyId(newVocabulary);
     const isVocabularyExists = vocabularyList.some(
-      (vocab) =>
-        vocab.character === newVocabulary.character &&
-        vocab.pronunciation === newVocabulary.pronunciation &&
-        vocab.translation === newVocabulary.translation &&
-        vocab.japanese === newVocabulary.japanese &&
-        vocab.translation.english === newVocabulary.translation.english &&
-        vocab.translation.german === newVocabulary.translation.german
+      (vocab) => generateVocabularyId(vocab) === newVocabularyId
     );
 
     if (!isVocabularyExists) {
-      // Wenn die Vokabel noch nicht vorhanden ist, füge sie hinzu
-      newVocabulary.id = Date.now();
-      setVocabularyList([...vocabularyList, newVocabulary]);
+      const updatedVocabularyList = [
+        ...vocabularyList,
+        { ...newVocabulary, id: Date.now() },
+      ];
+      setVocabularyList(updatedVocabularyList);
     } else {
       console.log('Vocabulary already exists:', newVocabulary);
       alert('Vocabulary already in your list!');
@@ -33,31 +33,31 @@ export const VocabularyProvider = ({ children }) => {
     setVocabularyList(updatedList);
   };
 
-  const isVocabularySelected = (item, language, alphabet) => {
-    console.log("HELLOOO", item, language);
-
+  const isVocabularySelected = (item, language) => {
     const isSelected = vocabularyList.some((vocab) => {
       if (language === 'german') {
         return (
           vocab.japanese === item.japanese &&
           vocab.pronunciation === item.pronunciation &&
+          vocab.translation &&
           vocab.translation.german === item.translation.german
-          );
-        }
-        if (language === 'english') {
-          return (
-            vocab.japanese === item.japanese &&
-            vocab.pronunciation === item.pronunciation &&
-            vocab.translation.english === item.translation.english
-            );
-          }
-          if (item) {
-            return (
-              vocab.character === item.character &&
-              vocab.pronunciation === item.pronunciation &&
-              vocab.translation === item.translation
-            );
-          }
+        );
+      }
+      if (language === 'english') {
+        return (
+          vocab.japanese === item.japanese &&
+          vocab.pronunciation === item.pronunciation &&
+          vocab.translation &&
+          vocab.translation.english === item.translation.english
+        );
+      }
+      if (item) {
+        return (
+          vocab.character === item.character &&
+          vocab.pronunciation === item.pronunciation &&
+          vocab.translation === item.translation
+        );
+      }
       return false;
     });
     return isSelected;
